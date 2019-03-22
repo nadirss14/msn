46fd13600d4ca53488bf4c4fe29cd08f4d0a7f0b
Nadir Soza <nadirss14@yahoo.es>
2019-03-22 15:05:19 -0600

    funcionalidad basica para creacion de cuenta

--- app/src/main/java/com/nadirss14/msn/LoginActivity.java
+++ app/src/main/java/com/nadirss14/msn/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.nadirss14.msn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.nadirss14.msn.views.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public  void goCreateAccount(View view ){
+        Intent intent=new Intent(LoginActivity.this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/nadirss14/msn/views/CreateAccountActivity.java
+++ app/src/main/java/com/nadirss14/msn/views/CreateAccountActivity.java
@@ -2,8 +2,12 @@ package com.nadirss14.msn.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.nadirss14.msn.R;
+
+import java.util.function.ToLongBiFunction;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -11,5 +15,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.my_toolbar_title_create_account),false);
     }
+
+    public void showToolbar(String _title,boolean _backBtn){
+        Toolbar tollbar=(Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(tollbar);
+        getSupportActionBar().setTitle(_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(_backBtn);
+    }
+
+
 }
