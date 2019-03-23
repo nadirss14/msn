ba534aa489f8bcb1fa275caba2e40128e2e13f9e
Nadir A. Soza <nadirss14@gmail.com>
2019-03-23 17:22:28 -0600

    Se agrega cardview, navigationbottomview, menu e imagenes

--- app/src/main/java/com/nadirss14/msn/views/CreateAccountActivity.java
+++ app/src/main/java/com/nadirss14/msn/views/CreateAccountActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+
 import com.nadirss14.msn.R;
 
 import java.util.function.ToLongBiFunction;
@@ -15,14 +16,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-        showToolbar(getResources().getString(R.string.my_toolbar_title_create_account),false);
+       showToolbar(getResources().getString(R.string.my_toolbar_title_create_account),true);
+
     }
 
-    public void showToolbar(String _title,boolean _backBtn){
+    public void showToolbar(String _title,boolean _backUp){
         Toolbar tollbar=(Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(tollbar);
         getSupportActionBar().setTitle(_title);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(_backBtn);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(_backUp);
     }
 
 
---
+++ app/src/main/java/com/nadirss14/msn/views/fragments/SearchCardFragment.java
@@ -0,0 +1,30 @@
+package com.nadirss14.msn.views.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nadirss14.msn.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchCardFragment extends Fragment {
+
+
+    public SearchCardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_card, container, false);
+    }
+
+}
